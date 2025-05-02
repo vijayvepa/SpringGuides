f0696ecff0ba05e38fc3af318ff3e46c43fd0f70
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-05-01 20:12:05 -0400

    spring init caching

---
+++ Cache.sh
@@ -0,0 +1,1 @@
+spring init --artifact-id caching --java-version 24 --name caching --type maven-project --dependencies cache
---
+++ Transactions.sh
@@ -0,0 +1,1 @@
+spring init --artifact-id transactions --java-version 24 --name transactions --type maven-project --dependencies h2,jdbc
