c68a3cf50bab78cffbce7c7aea0bc4ff2ec60de2
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-05-03 08:34:14 -0400

    Rest Data Access

---
+++ RestDataAccess/src/main/kotlin/com/example/accessing_data_rest/AccessingDataRestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.accessing_data_rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccessingDataRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccessingDataRestApplication>(*args)
+}
